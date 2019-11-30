14bd4f690b8224241829ae0763c579132eb8538d
koba <38810208+kkkobbb@users.noreply.github.com>
2019-11-30 18:02:56 +0900

    コード整理

--- Comet2/Comet2ISelLowering.cpp
+++ Comet2/Comet2ISelLowering.cpp
@@ -71,158 +71,12 @@ Comet2TargetLowering::Comet2TargetLowering(const TargetMachine &TM,
   setBooleanContents(ZeroOrOneBooleanContent);
 }
 
-
-// NOTE 定義あり llvm/lib/CodeGen/TargetLoweringBase.cpp
-#if 0
-EVT Comet2TargetLowering::getSetCCResultType(const DataLayout &DL, LLVMContext &,
-                                            EVT VT) const {
-  // TODO RISCVのまま
-  if (!VT.isVector())
-    return getPointerTy(DL);
-  return VT.changeVectorElementTypeToInteger();
-}
-#endif
-
-// NOTE 定義あり llvm/lib/CodeGen/TargetLoweringBase.cpp
-#if 0
-bool Comet2TargetLowering::isLegalAddressingMode(const DataLayout &DL,
-                                                const AddrMode &AM, Type *Ty,
-                                                unsigned AS,
-                                                Instruction *I) const {
-  // TODO RISCVのまま
-  // No global is ever allowed as a base.
-  if (AM.BaseGV)
-    return false;
-
-  // Require a 12-bit signed offset.
-  if (!isInt<12>(AM.BaseOffs))
-    return false;
-
-  switch (AM.Scale) {
-  case 0: // "r+i" or just "i", depending on HasBaseReg.
-    break;
-  case 1:
-    if (!AM.HasBaseReg) // allow "r+i".
-      break;
-    return false; // disallow "r+r" or "r+r+i".
-  default:
-    return false;
-  }
-
-  return true;
-}
-#endif
-
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-bool Comet2TargetLowering::isLegalICmpImmediate(int64_t Imm) const {
-  return isInt<12>(Imm);
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-bool Comet2TargetLowering::isLegalAddImmediate(int64_t Imm) const {
-  return isInt<12>(Imm);
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-// On RV32, 64-bit integers are split into their high and low parts and held
-// in two different registers, so the trunc is free since the low register can
-// just be used.
-bool Comet2TargetLowering::isTruncateFree(Type *SrcTy, Type *DstTy) const {
-  if (Subtarget.is64Bit() || !SrcTy->isIntegerTy() || !DstTy->isIntegerTy())
-    return false;
-  unsigned SrcBits = SrcTy->getPrimitiveSizeInBits();
-  unsigned DestBits = DstTy->getPrimitiveSizeInBits();
-  return (SrcBits == 64 && DestBits == 32);
-}
-
-bool Comet2TargetLowering::isTruncateFree(EVT SrcVT, EVT DstVT) const {
-  if (Subtarget.is64Bit() || SrcVT.isVector() || DstVT.isVector() ||
-      !SrcVT.isInteger() || !DstVT.isInteger())
-    return false;
-  unsigned SrcBits = SrcVT.getSizeInBits();
-  unsigned DestBits = DstVT.getSizeInBits();
-  return (SrcBits == 64 && DestBits == 32);
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-bool Comet2TargetLowering::isZExtFree(SDValue Val, EVT VT2) const {
-  // Zexts are free if they can be combined with a load.
-  if (auto *LD = dyn_cast<LoadSDNode>(Val)) {
-    EVT MemVT = LD->getMemoryVT();
-    if ((MemVT == MVT::i8 || MemVT == MVT::i16 ||
-         (Subtarget.is64Bit() && MemVT == MVT::i32)) &&
-        (LD->getExtensionType() == ISD::NON_EXTLOAD ||
-         LD->getExtensionType() == ISD::ZEXTLOAD))
-      return true;
-  }
-
-  return TargetLowering::isZExtFree(Val, VT2);
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-bool Comet2TargetLowering::isSExtCheaperThanZExt(EVT SrcVT, EVT DstVT) const {
-  return Subtarget.is64Bit() && SrcVT == MVT::i32 && DstVT == MVT::i64;
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-bool Comet2TargetLowering::hasBitPreservingFPLogic(EVT VT) const {
-  return false;
-}
-#endif
-
 SDValue Comet2TargetLowering::LowerOperation(SDValue Op,
                                              SelectionDAG &DAG) const {
   // Custum に指定したノードに対する操作
   report_fatal_error("unimplemented operand");
   return SDValue();
 }
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-// customに登録されていて、戻り値がillegalのノードで呼び出される
-void Comet2TargetLowering::ReplaceNodeResults(SDNode *N,
-                                             SmallVectorImpl<SDValue> &Results,
-                                             SelectionDAG &DAG) const {
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-SDValue Comet2TargetLowering::PerformDAGCombine(SDNode *N,
-                                               DAGCombinerInfo &DCI) const {
-  return SDValue();
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-bool Comet2TargetLowering::isDesirableToCommuteWithShift(
-    const SDNode *N, CombineLevel Level) const {
-  return true;
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-// 符号ビットに関する追加情報が必要な場合に実装する
-unsigned Comet2TargetLowering::ComputeNumSignBitsForTargetNode(
-    SDValue Op, const APInt &DemandedElts, const SelectionDAG &DAG,
-    unsigned Depth) const {
-  return 1;
-}
-#endif
 
 // NOTE 宣言あり llvm/include/llvm/CodeGen/TargetLowering.h
 MachineBasicBlock *
@@ -232,33 +86,6 @@ Comet2TargetLowering::EmitInstrWithCustomInserter(MachineInstr &MI,
   llvm_unreachable("Unexpected instr type to insert");
   return BB;
 }
-
-// TODO 不要？
-// Calling Convention Implementation.
-// The expectations for frontend ABI lowering vary from target to target.
-// Ideally, an LLVM frontend would be able to avoid worrying about many ABI
-// details, but this is a longer term goal. For now, we simply try to keep the
-// role of the frontend as simple and well-defined as possible. The rules can
-// be summarised as:
-// * Never split up large scalar arguments. We handle them here.
-// * If a hardfloat calling convention is being used, and the struct may be
-// passed in a pair of registers (fp+fp, int+fp), and both registers are
-// available, then pass as two separate arguments. If either the GPRs or FPRs
-// are exhausted, then pass according to the rule below.
-// * If a struct could never be passed in registers or directly in a stack
-// slot (as it is larger than 2*XLEN and the floating point rules don't
-// apply), then pass it using a pointer with the byval attribute.
-// * If a struct is less than 2*XLEN, then coerce to either a two-element
-// word-sized array or a 2*XLEN scalar (depending on alignment).
-// * The frontend can determine whether a struct is returned by reference or
-// not based on its size and fields. If it will be returned by reference, the
-// frontend must modify the prototype so a pointer with the sret annotation is
-// passed as the first argument. This is not necessary for large scalar
-// returns.
-// * Struct return values and varargs should be coerced to structs containing
-// register-size fields in the same situations they would be for fixed
-// arguments.
-
 
 // NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
 // Transform physical registers into virtual registers.
@@ -302,10 +129,11 @@ SDValue Comet2TargetLowering::LowerFormalArguments(
   for (unsigned i = 0, e = ArgLocs.size(); i != e; ++i) {
     CCValAssign &VA = ArgLocs[i];
     if (VA.isRegLoc()) {
+      LLVM_DEBUG(dbgs() << "### LowerFormalArguments Reg\n");
       // 引数がレジスタ経由で渡された場合
       EVT RegVT = VA.getLocVT();
       // 使用するレジスタクラスを指定
-      const TargetRegisterClass *RC = &Comet2::IntRegsRegClass;
+      const TargetRegisterClass *RC = &Comet2::IntRegsArgsRegClass;
       if (VA.getLocInfo() != CCValAssign::Full) {
         llvm_unreachable("not supported yet");
       }
@@ -316,6 +144,7 @@ SDValue Comet2TargetLowering::LowerFormalArguments(
       SDValue ArgValue = DAG.getCopyFromReg(Chain, DL, VReg, RegVT);
       InVals.push_back(ArgValue);
     } else {
+      LLVM_DEBUG(dbgs() << "### LowerFormalArguments Mem\n");
       // 引数がスタック経由で渡された場合
       assert(VA.isMemLoc());
 
@@ -361,18 +190,10 @@ SDValue Comet2TargetLowering::LowerCall(CallLoweringInfo &CLI,
 
   // TODO 未実装
 
+  // FIXME ここがおかしい？
+
   return Chain;
 }
-
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-bool Comet2TargetLowering::CanLowerReturn(
-    CallingConv::ID CallConv, MachineFunction &MF, bool IsVarArg,
-    const SmallVectorImpl<ISD::OutputArg> &Outs, LLVMContext &Context) const {
-  return true;
-}
-#endif
 
 SDValue
 Comet2TargetLowering::LowerReturn(SDValue Chain, CallingConv::ID CallConv,
@@ -434,116 +255,3 @@ const char *Comet2TargetLowering::getTargetNodeName(unsigned Opcode) const {
   }
   return nullptr;
 }
-
-// NOTE 定義あり llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
-#if 0
-/// getConstraintType - Given a constraint letter, return the type of
-/// constraint it is for this target.
-Comet2TargetLowering::ConstraintType
-Comet2TargetLowering::getConstraintType(StringRef Constraint) const {
-  return TargetLowering::getConstraintType(Constraint);
-}
-#endif
-
-// NOTE 定義あり llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
-#if 0
-std::pair<unsigned, const TargetRegisterClass *>
-Comet2TargetLowering::getRegForInlineAsmConstraint(const TargetRegisterInfo *TRI,
-                                                  StringRef Constraint,
-                                                  MVT VT) const {
-  return TargetLowering::getRegForInlineAsmConstraint(TRI, Constraint, VT);
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-unsigned
-Comet2TargetLowering::getInlineAsmMemConstraint(StringRef ConstraintCode) const {
-  return TargetLowering::getInlineAsmMemConstraint(ConstraintCode);
-}
-#endif
-
-// NOTE 定義あり llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
-#if 0
-void Comet2TargetLowering::LowerAsmOperandForConstraint(
-    SDValue Op, std::string &Constraint, std::vector<SDValue> &Ops,
-    SelectionDAG &DAG) const {
-  TargetLowering::LowerAsmOperandForConstraint(Op, Constraint, Ops, DAG);
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-Instruction *Comet2TargetLowering::emitLeadingFence(IRBuilder<> &Builder,
-                                                   Instruction *Inst,
-                                                   AtomicOrdering Ord) const {
-  return nullptr;
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-Instruction *Comet2TargetLowering::emitTrailingFence(IRBuilder<> &Builder,
-                                                    Instruction *Inst,
-                                                    AtomicOrdering Ord) const {
-  return nullptr;
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-TargetLowering::AtomicExpansionKind
-Comet2TargetLowering::shouldExpandAtomicRMWInIR(AtomicRMWInst *AI) const {
-  return AtomicExpansionKind::None;
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-Value *Comet2TargetLowering::emitMaskedAtomicRMWIntrinsic(
-    IRBuilder<> &Builder, AtomicRMWInst *AI, Value *AlignedAddr, Value *Incr,
-    Value *Mask, Value *ShiftAmt, AtomicOrdering Ord) const {
-  return nullptr;
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-TargetLowering::AtomicExpansionKind
-Comet2TargetLowering::shouldExpandAtomicCmpXchgInIR(
-    AtomicCmpXchgInst *CI) const {
-  return AtomicExpansionKind::None;
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-Value *Comet2TargetLowering::emitMaskedAtomicCmpXchgIntrinsic(
-    IRBuilder<> &Builder, AtomicCmpXchgInst *CI, Value *AlignedAddr,
-    Value *CmpVal, Value *NewVal, Value *Mask, AtomicOrdering Ord) const {
-  return nullptr;
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-unsigned Comet2TargetLowering::getExceptionPointerRegister(
-    const Constant *PersonalityFn) const {
-  return 0;
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-unsigned Comet2TargetLowering::getExceptionSelectorRegister(
-    const Constant *PersonalityFn) const {
-  return 0;
-}
-#endif
-
-// NOTE 定義あり llvm/include/llvm/CodeGen/TargetLowering.h
-#if 0
-bool Comet2TargetLowering::shouldExtendTypeInLibCall(EVT Type) const {
-  return true;
-}
-#endif
--- Comet2/Comet2ISelLowering.h
+++ Comet2/Comet2ISelLowering.h
@@ -45,66 +45,6 @@ public:
   EmitInstrWithCustomInserter(MachineInstr &MI,
                               MachineBasicBlock *BB) const override;
 
-#if 0
-  bool isLegalAddressingMode(const DataLayout &DL, const AddrMode &AM, Type *Ty,
-                             unsigned AS,
-                             Instruction *I = nullptr) const override;
-  bool isLegalICmpImmediate(int64_t Imm) const override;
-  bool isLegalAddImmediate(int64_t Imm) const override;
-  bool isTruncateFree(Type *SrcTy, Type *DstTy) const override;
-  bool isTruncateFree(EVT SrcVT, EVT DstVT) const override;
-  bool isZExtFree(SDValue Val, EVT VT2) const override;
-  bool isSExtCheaperThanZExt(EVT SrcVT, EVT DstVT) const override;
-
-  bool hasBitPreservingFPLogic(EVT VT) const override;
-
-  void ReplaceNodeResults(SDNode *N, SmallVectorImpl<SDValue> &Results,
-                          SelectionDAG &DAG) const override;
-
-  SDValue PerformDAGCombine(SDNode *N, DAGCombinerInfo &DCI) const override;
-
-  unsigned ComputeNumSignBitsForTargetNode(SDValue Op,
-                                           const APInt &DemandedElts,
-                                           const SelectionDAG &DAG,
-                                           unsigned Depth) const override;
-
-
-  ConstraintType getConstraintType(StringRef Constraint) const override;
-
-  unsigned getInlineAsmMemConstraint(StringRef ConstraintCode) const override;
-
-  std::pair<unsigned, const TargetRegisterClass *>
-  getRegForInlineAsmConstraint(const TargetRegisterInfo *TRI,
-                               StringRef Constraint, MVT VT) const override;
-
-  void LowerAsmOperandForConstraint(SDValue Op, std::string &Constraint,
-                                    std::vector<SDValue> &Ops,
-                                    SelectionDAG &DAG) const override;
-
-  EVT getSetCCResultType(const DataLayout &DL, LLVMContext &Context,
-                         EVT VT) const override;
-
-  Instruction *emitLeadingFence(IRBuilder<> &Builder, Instruction *Inst,
-                                AtomicOrdering Ord) const override;
-  Instruction *emitTrailingFence(IRBuilder<> &Builder, Instruction *Inst,
-                                 AtomicOrdering Ord) const override;
-
-  bool isDesirableToCommuteWithShift(const SDNode *N,
-                                     CombineLevel Level) const override;
-
-  /// If a physical register, this returns the register that receives the
-  /// exception address on entry to an EH pad.
-  unsigned
-  getExceptionPointerRegister(const Constant *PersonalityFn) const override;
-
-  /// If a physical register, this returns the register that receives the
-  /// exception typeid on entry to a landing pad.
-  unsigned
-  getExceptionSelectorRegister(const Constant *PersonalityFn) const override;
-
-  bool shouldExtendTypeInLibCall(EVT Type) const override;
-#endif
-
 private:
   // Lower incoming arguments, copy physregs into vregs
   SDValue LowerFormalArguments(SDValue Chain, CallingConv::ID CallConv,
@@ -112,33 +52,12 @@ private:
                                const SmallVectorImpl<ISD::InputArg> &Ins,
                                const SDLoc &DL, SelectionDAG &DAG,
                                SmallVectorImpl<SDValue> &InVals) const override;
-#if 0
-  bool CanLowerReturn(CallingConv::ID CallConv, MachineFunction &MF,
-                      bool IsVarArg,
-                      const SmallVectorImpl<ISD::OutputArg> &Outs,
-                      LLVMContext &Context) const override;
-#endif
   SDValue LowerReturn(SDValue Chain, CallingConv::ID CallConv, bool IsVarArg,
                       const SmallVectorImpl<ISD::OutputArg> &Outs,
                       const SmallVectorImpl<SDValue> &OutVals, const SDLoc &DL,
                       SelectionDAG &DAG) const override;
   SDValue LowerCall(TargetLowering::CallLoweringInfo &CLI,
                     SmallVectorImpl<SDValue> &InVals) const override;
-
-#if 0
-  TargetLowering::AtomicExpansionKind
-  shouldExpandAtomicRMWInIR(AtomicRMWInst *AI) const override;
-  virtual Value *emitMaskedAtomicRMWIntrinsic(
-      IRBuilder<> &Builder, AtomicRMWInst *AI, Value *AlignedAddr, Value *Incr,
-      Value *Mask, Value *ShiftAmt, AtomicOrdering Ord) const override;
-  TargetLowering::AtomicExpansionKind
-  shouldExpandAtomicCmpXchgInIR(AtomicCmpXchgInst *CI) const override;
-  virtual Value *
-  emitMaskedAtomicCmpXchgIntrinsic(IRBuilder<> &Builder, AtomicCmpXchgInst *CI,
-                                   Value *AlignedAddr, Value *CmpVal,
-                                   Value *NewVal, Value *Mask,
-                                   AtomicOrdering Ord) const override;
-#endif
 };
 }
 
