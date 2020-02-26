1ba9f29e13b035cabf3813b1990b9fbe47c6631a
koba <38810208+kkkobbb@users.noreply.github.com>
2020-02-26 19:05:00 +0900

    ラベルに追加する命令をNOPからDS 0に変更
    NOPだと1命令分無駄にするのでサイズ0のDS 0に変更

--- Comet2/MCTargetDesc/Comet2MCAsmInfo.cpp
+++ Comet2/MCTargetDesc/Comet2MCAsmInfo.cpp
@@ -23,7 +23,7 @@ void Comet2MCAsmInfo::anchor() {}
 Comet2MCAsmInfo::Comet2MCAsmInfo(const Triple &TT) {
   CodePointerSize = CalleeSaveStackSlotSize = 2;
   CommentString = ";";
-  LabelSuffix = " NOP ";  // ラベル単体は不可なのでNOPを付ける
+  LabelSuffix = " DS 0 ";  // ラベル単体は不可
   Data16bitsDirective = "\t.word\t";
   // TODO その他の設定は不要？
   /*
